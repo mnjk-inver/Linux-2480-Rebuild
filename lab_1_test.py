06ae456e5de317fbf5bcfe86a2199ceeee984d23
teucer12 <67879443+teucer12@users.noreply.github.com>
2021-02-14 11:36:54 -0600

    Update lab_1_test.py

--- lab_1_test.py
+++ lab_1_test.py
@@ -2,12 +2,22 @@
 # This command will import the OS library allowing linux CLI commands
 import subprocess
 
+
+def completion():
+    print("You have completed "+str(done)+" of "+str(total)+" tasks for this lab.")
+    if done == total:
+        print("Congratulations you have completed all tasks for this lab")
+
+
+done = 0
+total = 4
 # get the OS version
 hostnamectl = subprocess.run("hostnamectl", capture_output=True, text=True)
 
 # check if string exists in output
 if "Operating System: Debian GNU/Linux 10 (buster)" in hostnamectl.stdout:
     print("OS is Debian Buster")
+    done = done + 1
 else:
     print("You have installed the wrong distribution")
 
@@ -17,6 +27,7 @@ scan = subprocess.run("/usr/bin/nmap localhost", capture_output=True, text=True,
 # check if string exists in output
 if "22/tcp    open  ssh" in scan.stdout:
     print("SSH port has been opened")
+    done = done + 1
 else:
     print("SSH is not functional")
 
@@ -27,5 +38,16 @@ sudoer = subprocess.run("getent group sudo", capture_output=True, text=True, she
 # check if username is in sudoers
 if username in sudoer.stdout:
     print(username+" can execute commands with sudo.")
+    done = done + 1
 else:
     print(username+" is not a member of sudo group.")
+
+openvmtools_version = subprocess.run("apt list open-vm-tools", capture_output=True, text=True, shell=True)
+
+if "open-vm-tools/stable 2:10.3.10-1+deb10u2 amd64" in openvmtools_version.stdout:
+    print("You have installed the latest version of VM-Tools")
+    done = done + 1
+else:
+    print("You have not installed the latest version of VM-Tools.")
+
+completion()
