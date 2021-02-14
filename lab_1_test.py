daaa41cb0e6327f042cc6aa4be7d3ef552e2d5f3
mnjk-inver <77371448+mnjk-inver@users.noreply.github.com>
2021-02-14 12:34:48 -0600

    Update lab_1_test.py

--- lab_1_test.py
+++ lab_1_test.py
@@ -44,7 +44,7 @@ else:
 
 openvmtools_version = subprocess.run("apt list open-vm-tools", capture_output=True, text=True, shell=True)
 
-if "open-vm-tools" and "stable 2:10.3.10-1+deb10u2 amd64" in openvmtools_version.stdout:
+if "open-vm-tools/stable,now 2:10.3.10-1+deb10u2 amd64 [installed]" in openvmtools_version.stdout:
     print("You have installed the latest version of VM-Tools")
     done = done + 1
 else:
