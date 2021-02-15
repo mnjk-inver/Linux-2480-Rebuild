1d858a73dacc20aad8a6325b71401b61a7a68547
mnjk-inver <77371448+mnjk-inver@users.noreply.github.com>
2021-02-15 14:57:04 -0600

    Update lab_1_test.py

--- lab_1_test.py
+++ lab_1_test.py
@@ -1,55 +1,55 @@
-# This script will check ITC2480 Lab 1 for successful completion
-# This command will import the OS library allowing linux CLI commands
+#!/usr/bin/python
+# Import the subprocess module to run commands in shell
 import subprocess
+
+# Used for telling the user what is installed and not installed at the end
+os_debian = False
+ssh_open = False
+user_sudo = False
+vm_tools = False
+
+# Auto grab username of currently logged in user
+who_output = subprocess.run("who", capture_output=True, text=True, shell=True)
+who_split = who_output.stdout.split()
+username = str(who_split[0])
+
+# Check if debian is installed
+os_version = subprocess.run("cat /etc/issue", capture_output=True, text=True, shell=True)
+
+# Check if localhost has port 22 open
+ssh_on = subprocess.run("nc -z -v localhost 22", capture_output=True, text=True, shell=True)
+
+# Grab a list of groups current user is in
+is_sudo = subprocess.run("groups " + username, capture_output=True, text=True, shell=True)
+
+# Check if open vm tools is installed
+open_vm_tools = subprocess.run("dpkg -s open-vm-tools", capture_output=True, text=True, shell=True)
+
+if "Debian" in os_version.stdout:
+    os_debian = True
+    pass
+if "(ssh) open" in ssh_on.stderr:
+    ssh_open = True
+    pass
+if "sudo" in is_sudo.stdout:
+    user_sudo = True
+    pass
+if "Status: install ok installed" in open_vm_tools.stdout:
+    vm_tools = True
+    pass
 
 
 def completion():
-    print("You have completed "+str(done)+" of "+str(total)+" tasks for this lab.")
-    if done == total:
-        print("Congratulations you have completed all tasks for this lab")
+    if os_debian and ssh_open and user_sudo and vm_tools is True:
+        print("Everything is installed correctly. Great Work!")
+    if os_debian is False:
+        print("Debian is not installed!")
+    if ssh_open is False:
+        print("Port 22/SSH is not open!")
+    if user_sudo is False:
+        print("You are not a sudo user!")
+    if vm_tools is False:
+        print("Open-vm-tools is not installed!")
 
-
-done = 0
-total = 4
-# get the OS version
-hostnamectl = subprocess.run("hostnamectl", capture_output=True, text=True)
-
-# check if string exists in output
-if "Operating System: Debian GNU/Linux 10 (buster)" in hostnamectl.stdout:
-    print("OS is Debian Buster")
-    done = done + 1
-else:
-    print("You have installed the wrong distribution")
-
-# scan for open port
-scan = subprocess.run("/usr/bin/nmap localhost", capture_output=True, text=True, shell=True)
-
-# check if string exists in output
-if "22/tcp    open  ssh" in scan.stdout:
-    print("SSH port has been opened")
-    done = done + 1
-else:
-    print("SSH is not functional")
-
-# check sudo membership
-username = input("enter your username: ")
-sudoer = subprocess.run("getent group sudo", capture_output=True, text=True, shell=True)
-
-# check if username is in sudoers
-if username in sudoer.stdout:
-    print(username+" can execute commands with sudo.")
-    done = done + 1
-else:
-    print(username+" is not a member of sudo group.")
-
-#check installed version of open-vm-tools
-openvmtools_version = subprocess.run("apt list open-vm-tools", capture_output=True, text=True, shell=True)
-
-#check if installed version is latest version as of 2/14/2021
-if "open-vm-tools" and "[installed]" in openvmtools_version.stdout:
-    print("You have installed the latest version of VM-Tools")
-    done = done + 1
-else:
-    print("You have not installed the latest version of VM-Tools.")
 
 completion()
