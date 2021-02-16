95c2a6b8ee56f2a4c0e0426169f0b43a6042ad38
teucer12 <67879443+teucer12@users.noreply.github.com>
2021-02-16 13:46:34 -0600

    Add files via upload

---
+++ lab_3_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# Import the subprocess module to run commands in shell
+import subprocess
+
+# Used for telling the user what is installed and not installed at the end
+directories_present = False
+redteam_ours = False
+redteam_redteam = False
+
+# grab directory listing of redteam
+redteam_directory = subprocess.run("ls -al /home/jsmith/redteam", capture_output=True, text=True, shell=True)
+
+# grab appropriate values for redteam and ours
+list = subprocess.run("/home/jsmith/redteam/ls -al", capture_output=True, text=True, shell=True)
+splitlist = list.stdout.split()
+
+
+# grab directory listing of jsmith
+jsmith_directory = subprocess.run("/home/jsmith/ls -al", capture_output=True, text=True, shell=True)
+splitlist2 = jsmith_directory.stdout.split()
+
+
+if "theplan" and "yours" and "mine" and "ours" in redteam_directory.stdout:
+    directories_present = True
+    pass
+if splitlist[32] = "redteam" and splitlist[37] = "ours" in ssh_on.stderr:
+    redteam_ours = True
+    pass
+if splitlist[86] and [splitlist2[91] = "redtean":
+    redteam_redteam = True
+    pass
+
+
+def completion():
+    if directories_present and redteam_ours and redteam_redteam is True:
+        print("Everything is installed correctly. Great Work!")
+    if directories_present is False:
+        print("Some required directories are missing")
+    if redteam_ours is False:
+        print("Redteam is not the owner of /home/jsmith/redteam/ours")
+    if redteam_redteam is False:
+        print("Redteam is not the owner of home/jsmith/redteam")
+
+completion()
