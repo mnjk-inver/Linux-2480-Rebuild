ebc8b31294487372799e38d746f98cf49549b4ac
teucer12 <67879443+teucer12@users.noreply.github.com>
2021-02-28 08:55:38 -0600

    added message about standup mail server

--- lab_09_test.py
+++ lab_09_test.py
@@ -1,11 +1,12 @@
 import requests, subprocess, os.path
 
 def completion():
-    print("------------------------------------------")
+    print("-" * 45)
     print("You have completed "+str(done)+" of "+str(total)+" tasks for this lab.")
     if done == total:
         print("Congratulations you have completed all tasks for this lab")
-    print("------------------------------------------")
+    print("-" * 45)
+
 
 done = 0
 total = 7
@@ -29,7 +30,8 @@ def version_check(app):
         pass
     return version
 
-print("------------------------------------------")
+
+print("-" * 45)
 
 apps = ["postfix","courier-pop","courier-imap", "fam"]
 for app in apps:
@@ -48,16 +50,16 @@ if "sysadmin:" in aliases.stdout:
 else:
     print("alias was NOT successfully created")
 
-#check for telnet port 25
+#check for SMTP port 25
 # scan for open port
 scan = subprocess.run("/usr/bin/nmap localhost", capture_output=True, text=True, shell=True)
 
 # check if string exists in output
 if "25/tcp" in scan.stdout:
-    print("Telnet port has been opened")
+    print("SMTP port has been opened")
     done = done + 1
 else:
-    print("Telnet port is NOT open")
+    print("SMTP port is NOT open")
 #verify postfix service is running
 services = subprocess.run("systemctl --type=service",  capture_output=True, text=True, shell=True)
 if "postfix" in services.stdout:
@@ -67,5 +69,8 @@ else:
     print("Postfix is NOT running")
 
 #email to standup server
+print("-" * 45)
+print("Don't forget to test your mail server by sending" )
+print("   an email to ping@itc2480.campus.ihitc.net")
 
 completion()
