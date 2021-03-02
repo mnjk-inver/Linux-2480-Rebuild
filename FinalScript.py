db2382ec0d997962d38617907d071b09c847f7a0
Kreid <Kreid.kolo@gmail.com>
2021-03-01 18:23:20 -0600

    FinalScript Update ("Working" version. NON-FINAL)

--- FinalScript.py
+++ FinalScript.py
@@ -18,9 +18,9 @@ STUVMID = input("Enter System ID:")
 # Get ip address
 IPfull = subprocess.run("ip address show", capture_output=True, text=True, shell=True)
 IP = IPfull.stdout[IPfull.stdout.find("inet 172.17.50.") +15: IPfull.stdout.find("/24")]
-ipadd = "172.17.50."+IP
+STUVMIP = "172.17.50."+IP
 
-blog_url = f"http://{ipadd}/blog"
+blog_url = f"http://{STUVMIP}/blog"
 
 # Define aptcheck for checking installed programs
 def aptcheck(installed, command, name):
@@ -29,15 +29,12 @@ def aptcheck(installed, command, name):
     else:
         print(Warning + f"{name} is NOT installed!")
 
-# Define 'cat' command for repeated command usage 
-def catcommand(command):
-    output = subprocess.run(f"cat {command}", capture_output=True, text=True, shell=True)
+
+# Define simple command for repeated command usage
+def simplecommand(command):
+    output = subprocess.run(f"{command}", capture_output=True, text=True, shell=True)
     print(output.stdout)
 
-# Define 'ls -l' command for repeated command usage (May be able to merge with catcommand?)
-def lscommand(command):
-    output = subprocess.run(f"ls -l {command}", capture_output=True, text=True, shell=True)
-    print(output.stdout)
 
 # Define 'ls -al' command for repeated command usage
 def lsalcommand(command):
@@ -45,6 +42,36 @@ def lsalcommand(command):
     output2 = subprocess.run(f"ls -al {command} | cat -n | head", capture_output=True, text=True, shell=True)
     output3 = subprocess.run(f"ls -al {command} | cat -n | tail", capture_output=True, text=True, shell=True)
     print(output.stdout, output2.stdout, output3.stdout)
+
+
+# Define 'locate' command for both head and tails content
+def locate(command):
+    Files = subprocess.run(f"locate {command}", capture_output=True, text=True, shell=True)
+    for i in Files.stdout.splitlines():
+        print(i + " Contents:")
+        content = subprocess.run(f"cat -n {i} | head", capture_output=True, text=True, shell=True, errors='ignore')
+        content2 = subprocess.run(f"cat -n {i} | tail", capture_output=True, text=True, shell=True, errors='ignore')
+        print(content.stdout, content2.stdout)
+        
+
+# Define 'locate' command for tail content
+def locatetail(command):
+    Files = subprocess.run(f"locate {command}", capture_output=True, text=True, shell=True)
+    for i in Files.stdout.splitlines():
+        print(i + " Contents:")
+        content = subprocess.run(f"cat -n {i} | tail", capture_output=True, text=True, shell=True, errors='ignore')
+        print(content.stdout)
+
+
+# Define 'locate' command for both head and tails tar content
+def locatetar(command):
+    Files = subprocess.run(f"locate {command}", capture_output=True, text=True, shell=True)
+    for i in Files.stdout.splitlines():
+        print(i + " Contents:")
+        content = subprocess.run(f"tar -ztvf {i} | cat -n | head", capture_output=True, text=True, shell=True, errors='ignore')
+        content2 = subprocess.run(f"tar -ztvf {i} | cat -n | tail", capture_output=True, text=True, shell=True, errors='ignore')
+        print(content.stdout, content2.stdout)
+
 
 # Checking installed programs (Going to add 'subprocess.run' to the definition later!)
 aptcheck("installed", subprocess.run("apt list locate", capture_output=True, text=True, shell=True), "locate")
@@ -54,10 +81,10 @@ aptcheck("installed", subprocess.run("apt list python3", capture_output=True, te
 aptcheck("installed", subprocess.run("apt list python3-requests", capture_output=True, text=True, shell=True), "python3-requests")
 
 # Checking hostname, sudoers, students, and user id's
-catcommand("/etc/passwd | grep linuxgeek")
-catcommand("/etc/hostname")
-catcommand("/etc/group | grep sudo")
-catcommand("/etc/group | grep students")
+simplecommand("cat /etc/passwd | grep linuxgeek")
+simplecommand("cat /etc/hostname")
+simplecommand("cat /etc/group | grep sudo")
+simplecommand("cat /etc/group | grep students")
 
 # Checking if host user has sudo permissions (Can be improved. Just a working example!)
 is_sudo = subprocess.run("groups " + STUUSER, capture_output=True, text=True, shell=True)
@@ -67,26 +94,70 @@ else:
     print("not sudo")
 
 
-# Print out files and there content from the locate command output (Works to fast and breaks. May need to be timed!)
-# Could add as a child definition to the "catcommand" definition
-Files = subprocess.run("locate recent-log", capture_output=True, text=True, shell=True)
-for i in Files.stdout.splitlines():
-    print(i + " Contents:")
-    content = subprocess.run(f"cat -n {i} | tail", capture_output=True, text=True, shell=True)
-    print(content.stdout)
+# Print out files and there content from the 'locate tail' command output
+locatetail("lrecent-log")
 
 # Checking and outputing the directorys shown
-lscommand("/home/linuxgeek/")
-lscommand(f"/home/{STUUSER}/")
-lscommand(f"/home/{STUUSER}/itcfinal/")
+simplecommand("ls -l /home/linuxgeek/")
+simplecommand(f"ls -l /home/{STUUSER}/")
+simplecommand(f"ls -l /home/{STUUSER}/itcfinal/")
 
-# This is a repeat of the "for loop" above (This WILL be changed! This is bad code design! Dont reuse same code blocks!)
-Files2 = subprocess.run("locate kernmsg.txt", capture_output=True, text=True, shell=True)
-for i in Files2.stdout.splitlines():
-    print(i + " Contents:")
-    content = subprocess.run(f"cat -n {i} | head", capture_output=True, text=True, shell=True)
-    content2 = subprocess.run(f"cat -n {i} | tail", capture_output=True, text=True, shell=True)
-    print(content.stdout, content2.stdout)
+# Gather the output using the locate command and get the head/tails content
+locate("kernmsg.txt")
 
+# Us the ls -al command to get the contents of the backups folder
 lsalcommand(f"/home/{STUUSER}/backups/orig-config/")
 lsalcommand(f"/home/{STUUSER}/backups/orig-config/etc/")
+
+# Getting the tail of the system users of the home and root
+locatetail("system-users | grep -E 'home|root'")
+
+# Gather the output using the locate command and get the head/tails content
+locatetar("systemlogs.tar.gz")
+
+simplecommand("ls -al /home/linuxgeek/")
+simplecommand("ls -al /home/linuxgeek/ | head")
+simplecommand("ls -al /home/linuxgeek/itcfinal-backups")
+simplecommand("ls -al /home/linuxgeek/itcfinal-backups | head")
+simplecommand("dpkg -s apache2 2>&1 | head -2")
+simplecommand("dpkg -s php 2>&1 | head -2")
+simplecommand("dpkg -s php-mysql 2>&1 | head -2")
+simplecommand("dpkg -s libapache2-mod-php 2>&1 | head -2")
+simplecommand("dpkg -s mariadb-server 2>&1 | head -2")
+simplecommand("dpkg -s wordpress 2>&1 | head -2")
+simplecommand("ls -l /var/www")
+simplecommand("ls -l /var/www/html")
+simplecommand("ls /var/www/html/blog")
+WPSITETITLE = simplecommand(f'curl -s {STUVMIP}/blog/ | grep -o "<title>[^<]*" | tail -c+8')
+simplecommand(f"echo Site Title: {WPSITETITLE}")
+simplecommand("echo Entry Titles: ")
+simplecommand(f"curl -s {STUVMIP}/blog/ | grep -o -P '(?<=<h. class=\"entry-title heading-size-1\">).*(?=</h.>)'")
+simplecommand("fdisk -l /dev/sdb")
+simplecommand("file -sL /dev/sdb*")
+simplecommand("mount 2>&1 | grep sd")
+simplecommand("cat /etc/fstab")
+simplecommand("dpkg -s samba 2>&1 | head -2")
+simplecommand("tail -20 /etc/samba/smb.conf")
+simplecommand(f"ls -l /home/{STUUSER}/Windows File Test/")
+simplecommand("ls -al /winshare/students")
+simplecommand("dpkg -s bind9 2>&1 | head -2")
+simplecommand("nslookup google.com 127.0.0.1")
+simplecommand(f"nslookup sba-{STUVMID}.itc2480.campus.ihitc.net 127.0.0.1")
+simplecommand(f"nslookup mymachine.sba-{STUVMID}.itc2480.campus.ihitc.net 127.0.0.1")
+simplecommand(f"nslookup mailserver.sba-{STUVMID}.itc2480.campus.ihitc.net 127.0.0.1")
+simplecommand(f"nslookup www.sba-{STUVMID}.itc2480.campus.ihitc.net 127.0.0.1")
+simplecommand(f"nslookup -q=MX sba-{STUVMID}.itc2480.campus.ihitc.net 127.0.0.1")
+simplecommand(f"cat /var/lib/bind/sba-{STUVMID}.itc2480.campus.ihitc.net.hosts")
+# simplecommand(f"cat /var/lib/bind/sba-{STUVMID}.itc2480.campus.ihitc.net.hosts")
+simplecommand("cat /etc/dhcp/dhcpd.conf 2>&1 | tail")
+simplecommand("firewall-cmd --list-all-zones")
+simplecommand("iptables-legacy -L")
+simplecommand("iptables-legacy -t nat -L -n -v")
+simplecommand("sysctl net.ipv4.ip_forward")
+myscript = subprocess.run("locate myscript", capture_output=True, text=True, shell=True)
+for i in myscript.stdout.splitlines():
+    print(i + " Contents:")
+    content = subprocess.run(f"ls -l {i}", capture_output=True, text=True, shell=True, errors='ignore')
+    content2 = subprocess.run(f"cat {i}", capture_output=True, text=True, shell=True, errors='ignore')
+    print(content.stdout, content2.stdout)
+print("#"*25, "END OF SCRIPT", "#"*25)
