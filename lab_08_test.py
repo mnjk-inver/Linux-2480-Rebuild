1f052858bead843022361bf4666d28677df23895
Penguinkred <32975130+Penguinkred@users.noreply.github.com>
2021-04-22 16:08:07 -0500

    Update lab_08_test.py
    Comments added

--- lab_08_test.py
+++ lab_08_test.py
@@ -2,31 +2,34 @@
 import subprocess
 # Could use regex in this to check domains ([A-Za-z0-9]+.itc2480.campus.ihitc.net)
 
+# Gather contects using cat command and assign a variable
 done = 0
 bind_options = subprocess.run("cat /etc/bind/named.conf.options", capture_output=True, text=True, shell=True)
 interfaces = subprocess.run("cat /etc/network/interfaces", capture_output=True, text=True, shell=True)
 host = subprocess.run("cat /var/lib/bind/*.itc2480.campus.ihitc.net.hosts", capture_output=True, text=True, shell=True)
 print("-" * 45)
 
-
+# Completion of lab check and output
 def completion():
     print("-" * 45, "\n" "You have completed", str(done), "out of 3 tasks for this lab!")
     if done == 3:
         print("Congratulations you have completed all tasks for this lab.", "\n", "-" * 45)
 
-
+# Look for ip in named.conf.options
 if "172.17.139.11" in bind_options.stdout:
     print("Good work. Bind file has ip 172.17.139.11 stored in it!")
     done += 1
 else:
     print("Bind file has wrong ip address stored!")
 
+# Look for ip in interfaces
 if "127.0.0.1" in interfaces.stdout:
     print("Good work. Interfaces file has ip 127.0.0.1 stored in it!")
     done += 1
 else:
     print("Interfaces file has wrong ip address stored!")
 
+# Look for records listed below in the .hosts file
 if "MX" and "CNAME" and "AAAA" in host.stdout:
     print("Good work. Mail, CNAME, and IPv6 address found in bind hosts file!")
     done += 1
