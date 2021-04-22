3f60efeda8da95ba608a87fb2d950aae722c651d
Penguinkred <32975130+Penguinkred@users.noreply.github.com>
2021-04-22 16:08:10 -0500

    Update lab_04_test.py
    Comments added

--- lab_04_test.py
+++ lab_04_test.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 import os.path, subprocess
 
+# Check for files and get boolean
 list_file = os.path.isfile("listfiles.txt")
 log_file = os.path.isfile("gzlogfiles.txt")
 done = 0
 
-
+# Completion of lab check and output
 def completion():
     print("You have completed", str(done), "out of 5 tasks for this lab!")
     if done == 5:
         print("Congratulations you have completed all tasks for this lab.")
 
-
+# Check for directories and assign them a variable
 dir1 = os.path.expanduser("~/documentation")
 dir2 = os.path.expanduser("~/experiments")
 read_html = os.path.expanduser("/var/www/html/index.html")
 locate_installed = subprocess.run("dpkg -s locate", capture_output=True, text=True, shell=True)
 print("-" * 45)
 
+# Check if the directories where found and print output
 if os.path.exists(dir1):
     print("Good work the directory ~/documentation was created.")
     done = done + 1
@@ -42,6 +44,7 @@ if "Status: install ok installed" in locate_installed.stdout:
 else:
     print("Make sure to install locate!")
 
+# Check if both the files exist then print output
 if list_file and log_file is True:
     print("Good work both the listfiles.txt and gzlogfiles.txt exists")
     done = done + 1
